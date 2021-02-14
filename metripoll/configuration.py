f1719e7bc27863d466f44662d6a46fb238b53f3a
Jake Everhart <jake.a.everhart@gmail.com>
2021-02-14 15:55:23 -0600

    fixes append issue with file output

--- metripoll/configuration.py
+++ metripoll/configuration.py
@@ -1,6 +1,8 @@
 import argparse
 from typing import List, Text
 from metripoll.metric import Metric
+from importlib.metadata import version
+
 
 class Configuration:
     def __init__(self):
@@ -8,7 +10,7 @@ class Configuration:
 
     def _load_config(self):
         parser = argparse.ArgumentParser(prog="metripoll")
-        parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
+        parser.add_argument('--version', action='version', version=version("metripoll"))
         parser.add_argument(
             "-u", "--url", help="URL to poll for metrics", required=True)
         parser.add_argument(
@@ -16,7 +18,8 @@ class Configuration:
         parser.add_argument("-o", "--output", help="Output file name")
         parser.add_argument(
             "-d", "--delimiter", help="Delimiter character to use for parsing metrics", default=".")
-        parser.add_argument("-t", "--timestamp", help="Include timestamps in polling output", action="store_true")
+        parser.add_argument(
+            "-t", "--timestamp", help="Include timestamps in polling output", action="store_true")
         parser.add_argument(
             "metrics", help="JSON metrics to be parsed from the response", nargs="+", action=MetricParseAction)
 
@@ -46,8 +49,8 @@ class Configuration:
         if self.arguments.output is None:
             print(text, flush=True)
         else:
-            with open(self.arguments.output, 'w') as writer:
-                writer.write(text)
+            with open(self.arguments.output, 'a') as writer:
+                writer.write("{}\n".format(text))
 
 
 class MetricParseAction(argparse.Action):
